b3c9ffb44b66df61e06af4610c420e5311b84178
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-03-05 18:22:36 +0800

    Add files via upload

---
+++ finalprojectsale.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+
+st.write("""
+# Flight
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
+    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
+    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    data = {'TV': sepal_length,
+            'Radio': sepal_width,
+            'Newspaper': petal_length}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+data = pd.read_csv("Advertising.csv")
+data = data.drop(['Unnamed: 0'],axis=1)
+X = data.drop(['Sales'],axis=1)
+Y = data.Sales
+
+clf = LinearRegression()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(Y)
+
+st.subheader('Prediction')
+#st.write(iris.target_names[prediction])
+st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
