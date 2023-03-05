8d6272b76f3b309f711420955ea507eb6c237784
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-03-05 18:36:22 +0800

    Update finalprojectsale.py

--- finalprojectsale.py
+++ finalprojectsale.py
@@ -10,12 +10,12 @@ st.write("""
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
-    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
-    data = {'TV': sepal_length,
-            'Radio': sepal_width,
-            'Newspaper': petal_length}
+    TV = st.sidebar.slider('TV', 0.7, 297, 10)
+    Radio = st.sidebar.slider('Radio', 0, 50, 10)
+    Newspaper = st.sidebar.slider('Newspaper', 1.0, 27, 10)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -33,7 +33,7 @@ clf = LinearRegression()
 clf.fit(X, Y)
 
 prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
+#prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 st.write(Y)
@@ -42,5 +42,5 @@ st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
 st.write(prediction)
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+#st.subheader('Prediction Probability')
+#st.write(prediction_proba)
