ed1bc303d1bee2bb6619c16e2ad8fe2f6c5d6e8d
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-08-11 15:19:09 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -2,6 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+# importing package
+import turtle
+
+# set the background color
+# of the turtle screen
+turtle.Screen().bgcolor("orange")
+
 st.header("My first Streamlit App")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
