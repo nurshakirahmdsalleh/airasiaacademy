8927a158a413fe19646e0923bfc1d2f3fb038395
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-11-01 17:21:55 +0800

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -27,4 +27,4 @@ loaded_model = pickle.load(open("sales-advertising-model.h5", "rb"))
 prediction = loaded_model.predict(df)
 
 st.subheader('Prediction')
-st.write(prediction[0])
+st.write(f"{prediction[0]:.2f}")
