de4d9ab0fa633db298b139d7874b3c3108e8968a
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-11-01 20:43:28 +0800

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -21,8 +21,7 @@ def user_input_features():
             'Radio': radio,
             'Newspaper': newspaper}
     features = pd.DataFrame(data, index=[0])
-    scaled_features = scalerFeatures.fit_transform(features)
-    return scaled_features
+    return features
 
 df = user_input_features()
 
