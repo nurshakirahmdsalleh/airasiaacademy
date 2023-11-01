cc494c0dd83c787972eab572c80d954405cc6a20
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-11-01 18:51:19 +0800

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -31,8 +31,9 @@ scalerSales = pickle.load(open("sales-scaler.pkl", "rb"))
 scaled_features = scalerFeatures.fit_transform(df)
 
 prediction = loaded_model.predict(scaled_features)
+dfprediction = pd.DataFrame(prediction[0])
 
-unscale_prediction = scalerSales.inverse_transform(pd.DataFrame(prediction[0]))
+unscale_prediction = scalerSales.inverse_transform(dfprediction)
 st.write(unscale_prediction)
 
 #st.subheader('Sales Prediction')
